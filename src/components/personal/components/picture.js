01d7c809bee7c58091f5c530d73784ffcf0a8ce6
pearpages <pere@soms.cat>
2017-05-04 03:48:38 +0200

    bootstrap and profile image

--- src/components/personal/components/picture.js
+++ src/components/personal/components/picture.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default class Picture extends React.Component {
     render () {
-        return (<img id="profile-picture" className="img-fluid center-block" src="/media/pere-pages.jpg" alt="Pere Pages" />);
+        return (<img id="profile-picture" className="img-fluid center-block" src="media/pere-pages.jpg" alt="Pere Pages" />);
     }
 }
