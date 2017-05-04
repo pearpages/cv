8dc3e036b499ae6f9b30fa8a9f87387133d588b7
pearpages <pere@soms.cat>
2017-05-04 12:05:53 +0200

    header, margins and links

--- src/components/personal/components/follow-me.js
+++ src/components/personal/components/follow-me.js
@@ -1,35 +1,62 @@
-import React from 'react'
+import React from 'react';
+
+const socialNetworks = [{
+    icon: 'github',
+    name: 'Github',
+    url: 'https://github.com/pearpages',
+    short_url: 'github.com/pearpages'
+},
+{
+    icon: 'twitter',
+    name: 'Twitter',
+    url: 'https://twitter.com/pearpages',
+    short_url: 'twitter.com/pearpages'
+},
+{
+    icon: 'pencil',
+    name: 'Blog',
+    url: 'https://pearpages.com',
+    short_url: 'pearpages.com'
+},
+{
+    icon: 'stack-overflow',
+    name: 'Stackoverflow',
+    url: 'http://stackoverflow.com/users/3955722/pere-pages',
+    short_url: 'stackoverflow.com/users/3955722/pere-pages'
+},
+{
+    icon: 'linkedin',
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/pearpages/',
+    short_url: 'www.linkedin.com/in/pearpages/'
+}];
 
 export default class FollowMe extends React.Component {
+
+    renderLinks() {
+        let links = [];
+        socialNetworks.forEach((v) => {
+            links.push((
+                <div className="follow" key={v.icon}>
+                    <a target="_blank" href={v.url}>
+                    <div className="social-link">
+                        <span className={"fa fa-" + v.icon}></span> {v.name}
+                    </div>
+                   {v.short_url}</a>
+                </div>
+            ));
+        });
+
+        return links;
+    }
+
     render() {
         return (
             <div className="col-md-12">
                 <h4>FOLLOW ME</h4>
 
-                <div className="follow">
-                    <div className="page"><span className="fa fa-github"></span> Github</div>
-                    <a target="_blank" href="https://github.com/pearpages">github.com/pearpages</a>
-                </div>
+                {this.renderLinks()}
 
-                <div className="follow">
-                    <div className="page"><span className="fa fa-twitter"></span> Twitter</div>
-                    <a target="_blank" href="https://twitter.com/pearpages">twitter.com/pearpages</a>
-                </div>
-
-                <div className="follow">
-                    <div className="page"><span className="fa fa-pencil"></span> Blog</div>
-                    <a target="_blank" href="https://pearpages.com">pearpages.com</a>
-                </div>
-
-                <div className="follow">
-                    <div className="page"><span className="fa fa-stack-overflow"></span> Stackoverflow</div>
-                    <a target="_blank" href="http://stackoverflow.com/users/3955722/pere-pages">stackoverflow.com/users/3955722/pere-pages</a>
-                </div>
-
-                <div className="follow">
-                    <div className="page"><span className="fa fa-linkedin"></span> Linkedin</div>
-                    <a target="_blank" href="https://www.linkedin.com/in/pearpages/">www.linkedin.com/in/pearpages/</a>
-                </div>
             </div>
         );
     }
