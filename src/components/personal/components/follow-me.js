6169333c446abdd0ace33cebe43f2b2e24dac885
pearpages <pere@soms.cat>
2017-05-04 11:13:43 +0200

    social networks

--- src/components/personal/components/follow-me.js
+++ src/components/personal/components/follow-me.js
@@ -7,18 +7,28 @@ export default class FollowMe extends React.Component {
                 <h4>FOLLOW ME</h4>
 
                 <div className="follow">
-                    <div className="page"><span  className="fa fa-github"></span> Github</div>
+                    <div className="page"><span className="fa fa-github"></span> Github</div>
                     <a target="_blank" href="https://github.com/pearpages">github.com/pearpages</a>
                 </div>
 
                 <div className="follow">
-                    <div className="page"><span  className="fa fa-twitter"></span> Twitter</div>
+                    <div className="page"><span className="fa fa-twitter"></span> Twitter</div>
                     <a target="_blank" href="https://twitter.com/pearpages">twitter.com/pearpages</a>
                 </div>
 
                 <div className="follow">
-                    <div className="page"><span  className="fa fa-pencil"></span> Blog</div>
+                    <div className="page"><span className="fa fa-pencil"></span> Blog</div>
                     <a target="_blank" href="https://pearpages.com">pearpages.com</a>
+                </div>
+
+                <div className="follow">
+                    <div className="page"><span className="fa fa-stack-overflow"></span> Stackoverflow</div>
+                    <a target="_blank" href="http://stackoverflow.com/users/3955722/pere-pages">stackoverflow.com/users/3955722/pere-pages</a>
+                </div>
+
+                <div className="follow">
+                    <div className="page"><span className="fa fa-linkedin"></span> Linkedin</div>
+                    <a target="_blank" href="https://www.linkedin.com/in/pearpages/">www.linkedin.com/in/pearpages/</a>
                 </div>
             </div>
         );
