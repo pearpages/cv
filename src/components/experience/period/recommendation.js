abcf33a66d98366d6fa21e4c1d30777422333e5f
pearpages <pere@soms.cat>
2017-05-05 16:33:10 +0200

    margins and recommendations

--- src/components/experience/period/recommendation.js
+++ src/components/experience/period/recommendation.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './recommendation.scss';
 
 export default class Recommendation extends React.Component {
-    render () {
+    render() {
         const re = this.props.data;
         return (
-        <div className="row recommendation">
-            <div className="col-2 person">
-                <img src={"media/"+re.colleague.image} alt={re.colleague.name}/>
+            <div className="row recommendation">
+                <div className="col-12 text">
+                    <div dangerouslySetInnerHTML={{ __html: re.text }} />
+                </div>
+                <div className="col-12 person">
+                    <div className="float-right">
+                        <img className="float-left" src={"media/" + re.colleague.image} alt={re.colleague.name} />
+                        <div className="info">
+                            <div className="name">{re.colleague.name}</div>
+                            <div className="position"><span className="fa fa-user"></span> {re.colleague.title}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col-10 text">
-                <div dangerouslySetInnerHTML={{ __html: re.text}} />
-            </div>
-        </div>
         );
     }
 }
