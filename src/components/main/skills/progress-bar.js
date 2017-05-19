3bcc9b6bf087c604d86589ee4ef5fb6afc3ee9a1
pearpages <pere@soms.cat>
2017-05-19 22:26:53 +0200

    stateless component

--- src/components/main/skills/progress-bar.js
+++ src/components/main/skills/progress-bar.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import './progress-bar.scss';
 
-export default class ProgressBar extends React.Component {
+export default function ProgressBar(props) {
 
-    render() {
-        const data = this.props.data;
-        return (
-            (this.props.mobile) ?
-        <div className="row">
-            <div className="col-sm-12">
-                <div className="progress">
-                    <div className="progress-bar mobile" role="progressbar" style={{ width: data.value }}>
-                        {data.name}
+    const data = props.data;
+    return (
+        (props.mobile) ?
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="progress">
+                        <div className="progress-bar mobile" role="progressbar" style={{ width: data.value }}>
+                            {data.name}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
             :
-        <div className="row">
-            <div className="col-sm-3">{data.name}</div>
-            <div className="col-sm-9">
-                <div className="progress">
-                    <div className="progress-bar" role="progressbar" style={{ width: data.value }}></div>
+            <div className="row">
+                <div className="col-sm-3">{data.name}</div>
+                <div className="col-sm-9">
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar" style={{ width: data.value }}></div>
+                    </div>
                 </div>
-            </div>
-        </div>);
-    }
+            </div>);
 }
