576cf455df8b9e1a52e7f4d346cdfd76751dd538
pearpages <pere@soms.cat>
2017-05-21 21:28:36 +0200

    education stateless component

--- src/components/main/education/index.js
+++ src/components/main/education/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import TitleWithImage from '../title-with-image';
 import './index.scss';
 
-export default class Education extends React.Component {
-    render() {
-        return (
-            <div>
-                <ul className="no-decoration">
-                    {this.props.data.map( (data,i) => {
-                        return (
-                            <li key={i}>
-                                <TitleWithImage data={{
-                                    icon: 'graduation-cap',
-                                    imageFile: data.image,
-                                    line1: data.description,
-                                    line2: data.name
-                                }} />
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>);
-    }
+export default function Education(props) {
+
+    return (
+        <div>
+            <ul className="no-decoration">
+                {props.data.map((data, i) => {
+                    return (
+                        <li key={i}>
+                            <TitleWithImage data={{
+                                icon: 'graduation-cap',
+                                imageFile: data.image,
+                                line1: data.description,
+                                line2: data.name
+                            }} />
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>);
+
 }
