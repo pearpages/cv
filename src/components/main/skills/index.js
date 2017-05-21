ff80a22629789e7f6384bb9883f4b83b229b7421
pearpages <pere@soms.cat>
2017-05-21 21:43:53 +0200

    skills stateless component

--- src/components/main/skills/index.js
+++ src/components/main/skills/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Stars from './stars';
 import AnimatedProgressBar from './progressbars/animated-progressbar';
 
-export default class Skills extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="row skills">
-                {this.props.data.stars.map( (data,i) => {
-                    return <Stars class="col-4" key={i} data={data}/>
+export default function Skills(props) {
+
+    return (
+        <div>
+            <div className="row skills">
+                {props.data.stars.map((data, i) => {
+                    return <Stars class="col-4" key={i} data={data} />
                 })}
-                </div>
-
-                <hr />
-
-                {this.props.data.bars.map( (data,i) => {
-                    return <AnimatedProgressBar name={data.name} value={data.value} identifier={i} key={i} mobile={this.props.mobile}/>
-                })}
-
             </div>
-        );
-    }
+
+            <hr />
+
+            {props.data.bars.map((data, i) => {
+                return <AnimatedProgressBar name={data.name} value={data.value} identifier={i} key={i} mobile={props.mobile} />
+            })}
+
+        </div>
+    );
+
 }
