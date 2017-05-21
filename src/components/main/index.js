8f390682911c213d7b5e786b0174f1b090720a54
pearpages <pere@soms.cat>
2017-05-21 21:51:19 +0200

    main stateless

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -6,31 +6,30 @@ import Period from './period';
 import ExperienceContainer from './experience-container';
 import './index.scss';
 
-export default class Main extends React.Component {
+export default function Main(props) {
 
-    render() {
-        return (
-            <div className="col-md-9 col-sm-12 experience">
-                <div id="summary" className="row anchor profile">
-                    <Summary data={this.props.summary} />
-                </div>
-
-                <ExperienceContainer id="skills" name="SKILLS">
-                    <Skills data={this.props.skills} mobile={this.props.mobile} />
-                </ExperienceContainer>
-
-                <ExperienceContainer id="education" name="EDUCATION">
-                    <Education data={this.props.education} />
-                </ExperienceContainer>
-
-                <ExperienceContainer id="experience" name="EXPERIENCE">
-                    <div className="resume">
-                        {this.props.experience.map((v) => {
-                            return <Period key={v.id} data={v} />
-                        })}
-                    </div>
-                </ExperienceContainer>
+    return (
+        <div className="col-md-9 col-sm-12 experience">
+            <div id="summary" className="row anchor profile">
+                <Summary data={props.summary} />
             </div>
-        );
-    }
+
+            <ExperienceContainer id="skills" name="SKILLS">
+                <Skills data={props.skills} mobile={props.mobile} />
+            </ExperienceContainer>
+
+            <ExperienceContainer id="education" name="EDUCATION">
+                <Education data={props.education} />
+            </ExperienceContainer>
+
+            <ExperienceContainer id="experience" name="EXPERIENCE">
+                <div className="resume">
+                    {props.experience.map((v) => {
+                        return <Period key={v.id} data={v} />
+                    })}
+                </div>
+            </ExperienceContainer>
+        </div>
+    );
+
 }
