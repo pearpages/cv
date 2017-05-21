b38d628c460651ce19b88942b9cd1a9c059059eb
pearpages <pere@soms.cat>
2017-05-21 21:46:51 +0200

    summary stateless

--- src/components/main/summary/index.js
+++ src/components/main/summary/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import AnimatedText from './animated-text';
 import './index.scss';
 
-export default class Summary extends React.Component {
-    render() {
-        const data = this.props.data;
-        return (
-            <div className="col-md-12">
-                <h1 className="name">{data.name}</h1>
-                <h3 className="title"><span className="fa fa-terminal"></span> <AnimatedText text={data.title} /></h3>
-                <div className="summary" dangerouslySetInnerHTML={{ __html: data.summary}}></div>
-            </div>
-        );
-    }
+export default function Summary(props) {
+
+    const data = props.data;
+    return (
+        <div className="col-md-12">
+            <h1 className="name">{data.name}</h1>
+            <h3 className="title"><span className="fa fa-terminal"></span> <AnimatedText text={data.title} /></h3>
+            <div className="summary" dangerouslySetInnerHTML={{ __html: data.summary }}></div>
+        </div>
+    );
+
 }
