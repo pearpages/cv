239eaffce2c033214b2c2b9a181cc82156064471
pearpages <pere@soms.cat>
2017-05-21 22:04:15 +0200

    link-with-icon stateless

--- src/components/secondary/link-with-icon/index.js
+++ src/components/secondary/link-with-icon/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-export default class LinkWithIcon extends React.Component {
-    render () {
-        const v = this.props.linkData;
-        return (<div className="follow">
-                    {(v.url !== "") ?
-                    <a className="external-link" target="_blank" href={v.url}>
-                    <div className="social-link">
-                        <span className={"fa fa-" + v.icon}></span> {v.name}
-                    </div>
-                   {v.short_url}</a>
-                   :
-                   <a href="javascript:void(0)" style={{cursor: 'not-allowed'}}>
-                    <div className="social-link">
-                        <span className={"fa fa-" + v.icon}></span> {v.name}
-                    </div>
-                   {v.short_url}</a>
-                   }
+export default function LinkWithIcon(props) {
 
-                </div>);
-    }
+    const v = props.linkData;
+    return (<div className="follow">
+        {(v.url !== "") ?
+            <a className="external-link" target="_blank" href={v.url}>
+                <div className="social-link">
+                    <span className={"fa fa-" + v.icon}></span> {v.name}
+                </div>
+                {v.short_url}</a>
+            :
+            <a href="javascript:void(0)" style={{ cursor: 'not-allowed' }}>
+                <div className="social-link">
+                    <span className={"fa fa-" + v.icon}></span> {v.name}
+                </div>
+                {v.short_url}</a>
+        }
+
+    </div>);
+
 }
