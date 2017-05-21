de07bce7a373ca0a631b623dead8692404abf8ba
pearpages <pere@soms.cat>
2017-05-21 21:48:24 +0200

    title-with-image stateless

--- src/components/main/title-with-image/index.js
+++ src/components/main/title-with-image/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './index.scss';
 
-export default class TitleWithImage extends React.Component {
-    render() {
-        const data = this.props.data;
-        return (
+export default function TitleWithImage(props) {
+
+    const data = props.data;
+    return (
         <div className="employer-header">
             <div className="employer-image float-left" ><img width="68px" src={"media/" + data.imageFile} alt={data.line2} /></div>
             <div className="name-and-title">
-                <h1><span className={"fa fa-"+data.icon}></span> {data.line1}</h1>
+                <h1><span className={"fa fa-" + data.icon}></span> {data.line1}</h1>
                 {(data.url) ?
-                <h2><a target="_blank" className="external-link" href={data.url}>{data.line2}</a></h2>
-                :
-                <h2>{data.line2}</h2>
+                    <h2><a target="_blank" className="external-link" href={data.url}>{data.line2}</a></h2>
+                    :
+                    <h2>{data.line2}</h2>
                 }
 
             </div>
         </div>);
-    }
+
 }
