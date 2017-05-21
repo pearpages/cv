999c80cde7bca2297105df40005ff060e23bf77e
pearpages <pere@soms.cat>
2017-05-21 21:59:09 +0200

    secondary stateless

--- src/components/secondary/index.js
+++ src/components/secondary/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import Sidebar from './sidebar';
 import './index.scss';
 
-export default class Secondary extends React.Component {
+export default function Secondary(props) {
 
-    render() {
-        return (
-            <div className="col-md-3 col-sm-12 personal">
-                <div id="picture" className={"row profile "+(this.props.mobile ? "anchor" : '')} >
-                    <div className={"col-md-12 " + (this.props.mobile === true ? 'mobile-picture' : null)}>
-                        <img id="profile-picture" className="img-fluid center-block" src="media/pere-pages.jpg" alt="Pere Pages" />
-                    </div>
+    return (
+        <div className="col-md-3 col-sm-12 personal">
+            <div id="picture" className={"row profile " + (props.mobile ? "anchor" : '')} >
+                <div className={"col-md-12 " + (props.mobile === true ? 'mobile-picture' : null)}>
+                    <img id="profile-picture" className="img-fluid center-block" src="media/pere-pages.jpg" alt="Pere Pages" />
                 </div>
+            </div>
 
-                <div id="contact" className="row section dark-blue">
-                    <Sidebar id="contact" name="CONTACT ME" mobile={this.props.mobile} data={this.props.contact} />
-                </div>
+            <div id="contact" className="row section dark-blue">
+                <Sidebar id="contact" name="CONTACT ME" mobile={props.mobile} data={props.contact} />
+            </div>
 
-                <div id="community" className="row section dark-blue">
-                    <Sidebar id="community" name="COMMUNITY" mobile={this.props.mobile} data={this.props.community} />
-                </div>
-            </div>);
-    }
+            <div id="community" className="row section dark-blue">
+                <Sidebar id="community" name="COMMUNITY" mobile={props.mobile} data={props.community} />
+            </div>
+        </div>);
+
 }
