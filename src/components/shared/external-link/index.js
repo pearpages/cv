1acf9abfb06f0ac2e27ac8afe23fd0922b1cf690
pearpages <pere@soms.cat>
2017-05-24 18:40:43 +0200

    external links

--- src/components/shared/external-link/index.js
+++ src/components/shared/external-link/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ExternalLink(props) {
 
     if (props.url !== "") {
-        return <a className="external-link" target="_blank" href={props.url}>
+        return <a className="external-link" target="_blank" rel="noopener" href={props.url}>
             {props.children}
         </a>
     } else {
