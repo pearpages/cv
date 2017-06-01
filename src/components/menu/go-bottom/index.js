98454834866d72ba521285c726c3ab69e2d3f2d1
pearpages <pere@soms.cat>
2017-06-01 10:25:17 +0200

    remove goBottom button from print version

--- src/components/menu/go-bottom/index.js
+++ src/components/menu/go-bottom/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 export default function GoBottom(props) {
 
     return (<div
+                className="no-print"
                 onClick={props.handleOnClick}
                 id="go-bottom">
                     <span className={"fa fa-angle-"+(props.bottom ? 'up' : 'down')}></span>
