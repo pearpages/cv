cc08883a40e221486d32b5ef9f084c4df877e49f
pearpages <pere@soms.cat>
2017-06-01 10:19:32 +0200

    fix stars

--- src/components/main/skills/stars/animated-stars.js
+++ src/components/main/skills/stars/animated-stars.js
@@ -7,6 +7,7 @@ export default class AnimatedStars extends React.Component {
         super(props);
         this.state = { value: 0 };
         this.recursive = this.recursive.bind(this);
+        this.running = false;
     }
 
     componentDidMount() {
@@ -18,8 +19,10 @@ export default class AnimatedStars extends React.Component {
     }
 
     recursive() {
-        if (this.state.value < this.props.value && this.props.visible) {
+        if ( (this.state.value < this.props.value) && this.props.visible && !this.running) {
+            this.running = true;
             setTimeout(() => {
+                this.running = false;
                 this.setState({ value: this.state.value + 0.5 });
             }, 200);
         }
