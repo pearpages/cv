481843c3842678826e26fbcee17068171cb29fc4
pearpages <pere@soms.cat>
2019-02-07 09:59:44 +0100

    feat: cv updated

--- src/components/main/period/stack/index.js
+++ src/components/main/period/stack/index.js
@@ -4,7 +4,7 @@ export default function Stack(props) {
 
     return (<div>
         <h3>
-        <span className="icon fa fa-align-justify"></span> Tech Stack</h3>
+        <span className="icon fa fa-align-justify"></span> Tech Stack / Technologies</h3>
         <div className="container">{props.stack}</div>
     </div>)
 
